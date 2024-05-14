5f73d45e0579e00c42763403369e298b2e40ac3b
SteveYM3111 <steveym3111@gmail.com>
2024-05-14 11:09:52 +0700

    loginAdmin

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\HomeController;
+Use App\Http\Controllers\webController;
+Use App\Http\Controllers\loginAdmin;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function (){
+//     return view ('welcome');
+// });
 
-Route::get('/', function () {
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('/user/{id}', function ($id){
+    return 'User dengan ID ' . $id;
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+/*Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+   return view('list_barang', compact('id','nama'));
+});*/
+
+Route::get('/home',[webController::class, 'home']);
+Route::get('/product',[webController::class, 'product']);
+Route::get('/login', [webController::class, 'login']);
+Route::get('/lokasi', [webController::class, 'lokasi']);
+Route::get('/loginAdmin', [loginAdmin::class, 'loginAdmin']);
